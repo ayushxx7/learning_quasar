1fedc1fa4d6374bbdf8f92a68375f7067cd8d4e9
Ayush <ayushxx7@gmail.com>
2020-08-18 23:16:06 +0530

    navbar made functional using `to`. created task items, added some styles, and on click checkmark will be toggled

--- todo/src/pages/Help.vue
+++ todo/src/pages/Help.vue
@@ -1,6 +1,8 @@
 <template>
-  <q-page class="">
-    <h5>Help</h5>
+  <q-page class="q-pa-lg">
+    <h5 class='q-mt-none'>Help</h5>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <p>Amet libero expedita modi deserunt accusamus necessitatibus. Placeat repudiandae illo animi nobis sed? Autem est culpa dolor commodi ratione? Expedita ipsa debitis mollitia distinctio sit? Veniam illum possimus architecto officiis!</p>
   </q-page>
 </template>
 
--- todo/src/pages/Todo.vue
+++ todo/src/pages/Todo.vue
@@ -1,11 +1,59 @@
 <template>
-  <q-page class="">
-    <h5>Todo</h5>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white"
+      separator
+      bordered>
+      <q-item 
+        v-for='task in tasks'
+        :key="task.title"
+        @click='task.done = !task.done'
+        clickable
+        :class="{ 'done': task.done }"
+        v-ripple>
+        <q-item-section avatar>
+          <q-checkbox 
+            class='no-pointer-events'
+            v-model="task.done" 
+            color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+      </q-item>
+
+    </q-list>  
   </q-page>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: "Get Oranage",
+          done: false
+        },
+        {
+          title: "Make Juice",
+          done: false
+        },
+        {
+          title: "Drink Juice",
+          done: false
+        },
+      ]
+    }
+  },
   name: 'PageIndex'
 }
 </script>
+
+<style>
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+
+  }
+}
+</style>
