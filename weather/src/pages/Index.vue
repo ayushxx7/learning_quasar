7dd39c22538712cb45c746241a0616b871d74070
Ayush Mandowara <ayushxx7@gmail.com>
2020-08-22 15:13:20 +0530

    feat: Starting to build a weather app.
    created a new qusar app using `quasar create`.
    Removed default Header and Nav bar, as this is a single page weather
    app. Gradient added from uigradients.com. Search bar UI mostly complete.

---
+++ weather/src/pages/Index.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-page class="flex column">
+    <div class='col q-pt-lg q-px-md'>
+      <q-input 
+        filled 
+        v-model="search" 
+        placeholder="Search" 
+        dark
+        borderless
+        >
+        <template v-slot:before>
+          <q-icon name="my_location" />
+        </template>
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="search" />
+        </template>
+      </q-input> 
+    </div>
+   <div class="col text-white text-center">
+
+   </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      search: ''
+    }
+  }
+
+}
+</script>
+
+<style lang='sass'>
+.q-page
+  background: linear-gradient(to bottom, #136a8a, #267871)
+</style>
